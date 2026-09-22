17a4f4657efc97ac4309e070698951a22d1c5146
sam_Rezaee <srezaie.c.e@gmail.com>
2026-09-22 23:18:21 +0330

    Start Task one(SRP), and create a class  that violates SRP

---
+++ HomeWork4/Task1(SRP)/ExampleClassWithoutSRP.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4.Task1_SRP_
+{
+    /// <summary>
+    /// SRP:
+    /// هر ماژول نرم افزاری یا کلاس باید فقط و فقط یک دلیل برای تغییر داشته باشد
+    /// اما در این کلاس چندین مسولیت که هر کدام میتواند سرویس جداگانه‌ای باشد همه یکجا دراین کلاس تعریف شده است
+    /// </summary>
+    public class ExampleClassWithoutSRP
+    {
+        public class OrderService
+        {
+            public string CreateOrder(string OrderDetails)
+            {
+                string OrderId = "";
+                //Code to Create Order
+                return OrderId;
+            }
+
+            public bool MakePayment(string OrderId)
+            {
+                //Code to Make Payment
+                return true;
+            }
+
+            public bool GenerateInvoice(string OrderId)
+            {
+                //Code to Generate Invoice
+                return true;
+            }
+
+            public bool EmailInvoice(string OrderId)
+            {
+                //Code to Email Invoice
+                return true;
+            }
+        }
+    }
+}
