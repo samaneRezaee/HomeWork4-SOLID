44691a5b82aac26298472771d7f66c0356447c5f
sam_Rezaee <srezaie.c.e@gmail.com>
2026-09-23 23:32:54 +0330

    write class Order in Module layer and write main method body

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace HomeWork4
+﻿using HomeWork4.Task7_Store_.Implementations;
+using HomeWork4.Task7_Store_.Module;
+
+namespace HomeWork4
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var discount = new PercentageDiscount(10); // 10% تخفیف
+            var fixedDiscount = new FixedAmountDiscount(200);//200$ تخفیف
+            var notifier = new EmailNotification();
+
+            var order = new Order(discount, notifier);
+            var orderFixed= new Order(fixedDiscount, notifier);
+
+            order.AddProduct(new Product("Television", 1000));
+            order.AddProduct(new Product("Computer", 2000));
+            orderFixed.AddProduct(new Product("Ruge", 950));
+
+            order.Checkout();
+            orderFixed.Checkout();
         }
     }
 }
---
+++ HomeWork4/Task7(Store)/Module/Order.cs
@@ -0,0 +1,33 @@
+﻿
+
+using HomeWork4.Task7_Store_.Interfaces;
+
+namespace HomeWork4.Task7_Store_.Module
+{
+    public class Order
+    {
+        private readonly IDiscountStrategy _discount;
+        private readonly INotificationService _notifier;
+        private readonly List<IProduct> _items = new List<IProduct>();
+
+   
+        public Order(IDiscountStrategy discount, INotificationService notifier)
+        {
+            _discount = discount;
+            _notifier = notifier;
+        }
+
+        public void AddProduct(IProduct product) => _items.Add(product);
+
+        public void Checkout()
+        {
+            decimal total = 0;
+            foreach (var item in _items) total += item.Price;
+
+            decimal finalPrice = _discount.ApplyDiscount(total);
+
+            Console.WriteLine($"Total: {total}, Final Price: {finalPrice}");
+            _notifier.Send("Invoice sent to customer.");
+        }
+    }
+}
