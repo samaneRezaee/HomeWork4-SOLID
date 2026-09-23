78552dacf1a1186ae16bd7bb4a6d89cc3efd595c
sam_Rezaee <srezaie.c.e@gmail.com>
2026-09-23 23:10:22 +0330

    write Implementation layer

---
+++ HomeWork4/Task7(Store)/Implementations/EmailNotification .cs
@@ -0,0 +1,11 @@
+﻿
+
+using HomeWork4.Task7_Store_.Interfaces;
+
+namespace HomeWork4.Task7_Store_.Implementations
+{
+    public class EmailNotification : INotificationService
+    {
+        public void Send(string message) => Console.WriteLine($"Email Service: {message}");
+    }
+}
