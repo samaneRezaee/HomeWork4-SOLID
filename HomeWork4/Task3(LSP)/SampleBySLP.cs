81074cbff8df0b1c3c9545f594b183bf681c45f5
sam_Rezaee <srezaie.c.e@gmail.com>
2026-09-23 18:59:25 +0330

    a class based on SLP

---
+++ HomeWork4/Task3(LSP)/SampleBySLP.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4.Task3_LSP_
+{
+    /// <summary>
+    /// هر کلاسی میتواند آنچه را که مدعی آن است را انحام دهد
+    /// </summary>
+    public class SampleBySLP
+    {
+        public interface IShape
+        {
+            int Area();
+        }
+
+        public class Rectangle : IShape
+        {
+            public int Width { get; set; }
+            public int Height { get; set; }
+            public int Area() => Width * Height;
+        }
+
+        public class Square : IShape
+        {
+            public int Side { get; set; }
+            public int Area() => Side * Side;
+        }
+        public static void PrintArea(IShape shape)
+        {
+            Console.WriteLine(shape.Area());
+        }
+        public static void Main(string[] args) 
+        {
+            PrintArea(new Rectangle { Width = 5, Height = 4 }); // 20
+            PrintArea(new Square { Side = 4 });                // 16
+        }
+    }
+}
