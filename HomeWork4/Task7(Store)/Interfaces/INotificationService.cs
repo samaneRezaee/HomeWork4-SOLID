dc65a16099063867886f5e8c0447bba4d9312189
sam_Rezaee <srezaie.c.e@gmail.com>
2026-09-23 22:58:15 +0330

    create interface layer task7(store)

---
+++ HomeWork4/Task7(Store)/Interfaces/INotificationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork4.Task7_Store_.Interfaces
+{
+    public interface INotificationService
+    {
+        void Send(string message);
+    }
+}
